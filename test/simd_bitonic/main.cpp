33fc114ad49f39ee715bd929bdbe3d8586c2143c
Geolm <moyaguillaume@gmail.com>
2021-11-03 22:12:13 -0400

    more profiling tests

--- test/simd_bitonic/main.cpp
+++ test/simd_bitonic/main.cpp
@@ -12,27 +12,23 @@
 
 #define ALIGNED_VARIABLE __attribute__((aligned(32)))
 #define NUMBER_OF_SORTS (10000000)
-#define ARRAY_SIZE (32)
+#define MAX_ARRAY_SIZE (32)
 
 int seed = 0x12345678;
 
-int main(int argc, const char * argv[])
+void profile(int array_size)
 {
-    ALIGNED_VARIABLE float array[ARRAY_SIZE];
-    
-    stm_setup();
-    
-    printf("profiling, %d times sorting an array of %d float values\n\n", NUMBER_OF_SORTS, ARRAY_SIZE);
+    ALIGNED_VARIABLE float array[MAX_ARRAY_SIZE];
     
     std::vector<float> vector;
-    vector.resize(ARRAY_SIZE);
+    vector.resize(array_size);
     
     uint64_t current_time = stm_now();
     float result = 0.f;
     
     for(int i=0; i<NUMBER_OF_SORTS; ++i)
     {
-        for(int j=0; j<ARRAY_SIZE; ++j)
+        for(int j=0; j<array_size; ++j)
             vector[j] = iq_random_float(&seed);
         
         std::sort(vector.begin(), vector.end());
@@ -42,26 +38,34 @@ int main(int argc, const char * argv[])
     
     uint64_t raw_delta_time = stm_since(current_time);
     float stl_duration = (float)stm_sec(raw_delta_time);
-    printf("std::vector sort, duration : %f seconds, result : %f\n", stl_duration, result);
-    
     
     current_time = stm_now();
-    result = 0.f;
+    float simd_result = 0.f;
     seed = 0x12345678;
     
     for(int i=0; i<NUMBER_OF_SORTS; ++i)
     {
-        for(int j=0; j<ARRAY_SIZE; ++j)
+        for(int j=0; j<array_size; ++j)
             array[j] = iq_random_float(&seed);
         
-        simd_sort_float(array, ARRAY_SIZE);
+        simd_sort_float(array, array_size);
         
-        result += array[0];
+        simd_result += array[0];
     }
     
     raw_delta_time = stm_since(current_time);
     float bitonic_duration = (float)stm_sec(raw_delta_time);
-    printf("simd_bitonic sort, duration : %f seconds, result : %f, %f times faster than stl\n", bitonic_duration, result, stl_duration/bitonic_duration);
+    printf("array of %d elements : simd_bitonic sort is %f times faster than stl\n", array_size, stl_duration/bitonic_duration);
+}
+
+int main(int argc, const char * argv[])
+{
+    
+    
+    stm_setup();
+    
+    for(int i=2; i<=32; ++i)
+        profile(i);
     
     return 0;
 }
